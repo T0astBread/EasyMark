874aa4fdb0d2fc0bf47f96af00109a12442ed246
T0astBread <tb@t0ast.cc>
2020-07-24 18:15:55 +0200

    Set up Pebble for templating

--- src/main/java/easymark/Main.java
+++ src/main/java/easymark/Main.java
@@ -1,13 +1,29 @@
 package easymark;
 
 import io.javalin.*;
+import io.javalin.plugin.rendering.*;
+import io.javalin.plugin.rendering.template.*;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        JavalinRenderer.register(JavalinPebble.INSTANCE, ".peb");
+
         Javalin app = Javalin.create();
 
         app.get("/", ctx -> {
-            ctx.result("hello world!");
+            String name = ctx.sessionAttribute("name");
+            Map<String, Object> model = new HashMap<>();
+            model.put("name", name);
+            model.put("stack", List.of("Java", "Jetty", "Javalin"));
+            ctx.render("hellow.peb", model);
+        });
+
+        app.post("/", ctx -> {
+            String name = ctx.formParam("name");
+            ctx.sessionAttribute("name", name);
+            ctx.redirect("/");
         });
 
         app.start(8080);
