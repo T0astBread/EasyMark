4efc86c496774852f4f15953bac654367eef4481
T0astBread <tb@t0ast.cc>
2020-08-15 02:16:06 +0200

    Implement participant course view

--- src/main/java/easymark/webserver/routes/IndexRoutes.java
+++ src/main/java/easymark/webserver/routes/IndexRoutes.java
@@ -41,6 +41,7 @@ public class IndexRoutes {
 
             Set<UserRole> roles = ctx.sessionAttribute(SessionKeys.ROLES);
             if (roles != null) {
+
                 if (roles.contains(UserRole.ADMIN)) {
                     UUID adminId = ctx.sessionAttribute(SessionKeys.ENTITY_ID);
                     model.put(ModelKeys.ADMIN_ID, adminId);
@@ -55,7 +56,52 @@ public class IndexRoutes {
                     }
                     ctx.render("pages/index.admin.peb", model);
                     return;
+
                 } else if (roles.contains(UserRole.PARTICIPANT)) {
+                    UUID participantId = ctx.sessionAttribute(SessionKeys.ENTITY_ID);
+                    Course course;
+                    List<Chapter> chapters;
+                    Map<UUID, List<Assignment>> assignmentsPerChapter;
+                    Map<UUID, AssignmentResult> assignmentResultPerAssignment = new HashMap<>();
+                    try (DatabaseHandle db = DBMS.openRead()) {
+                        UUID courseId = db.get().getParticipants()
+                                .stream()
+                                .filter(p -> p.getId().equals(participantId))
+                                .findAny()
+                                .orElseThrow(() -> new BadRequestResponse("You were not found in the database"))
+                                .getCourseId();
+                        course = db.get().getCourses()
+                                .stream()
+                                .filter(c -> c.getId().equals(courseId))
+                                .findAny()
+                                .orElseThrow(() -> new InternalServerErrorResponse("Course not found in database"));
+                        assignmentsPerChapter = new HashMap<>();
+                        chapters = db.get().getChapters()
+                                .stream()
+                                .filter(chapter -> chapter.getCourseId().equals(courseId))
+                                .peek(chapter -> {
+                                    List<Assignment> assignments = db.get().getAssignments()
+                                            .stream()
+                                            .filter(assignment -> assignment.getChapterId().equals(chapter.getId()))
+                                            .sorted(Comparator.comparingInt(Assignment::getOrdNum))
+                                            .collect(Collectors.toUnmodifiableList());
+                                    assignmentsPerChapter.put(chapter.getId(), assignments);
+
+                                    for (Assignment a : assignments) {
+                                        db.get().getAssignmentResults()
+                                                .stream()
+                                                .filter(ar -> ar.getParticipantId().equals(participantId) && a.getId().equals(ar.getAssignmentId()))
+                                                .findAny()
+                                                .ifPresent(ar -> assignmentResultPerAssignment.put(a.getId(), ar));
+                                    }
+                                })
+                                .sorted(Comparator.comparingInt(Chapter::getOrdNum))
+                                .collect(Collectors.toUnmodifiableList());
+                    }
+                    model.put(ModelKeys.COURSE, course);
+                    model.put(ModelKeys.CHAPTERS, chapters);
+                    model.put(ModelKeys.ASSIGNMENTS_PER_CHAPTER, assignmentsPerChapter);
+                    model.put(ModelKeys.ASSIGNMENT_RESULT_PER_ASSIGNMENT, assignmentResultPerAssignment);
                     ctx.render("pages/index.participant.peb", model);
                     return;
                 }
